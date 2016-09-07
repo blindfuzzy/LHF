ada19d2f5a16821d9285e4895bd73053940db23e
blindfuzzy <blindfuzzy@users.noreply.github.com>
2016-09-07 18:36:56 -0400

    Update httprecon.py

--- Modules/httprecon.py
+++ Modules/httprecon.py
@@ -20,7 +20,7 @@ try:
     print('\033[1;37m[-]  |     Starting HTTP script scan for {0}:{1} \033[1;m'.format(ip_address, port))
     print "\033[1;37m[-]  ----------------------------------------------------------------------------- \033[1;m"
 
-    if not recon.checknmaprunmod(ip_address, "_http.nmap.{0}".format(port, ip_address)):
+    if not recon.checknmaprunmod(ip_address, "_http.nmap.{0}".format(ip_address)):
         HTTPSCAN = "nmap -sV -Pn -vv -p {0} --script-args=unsafe=1 --script=http-enum,http-feed,http-open-proxy,http-headers,http-cors,http-server-header,http-php-version,http-form-brute,http-iis-short-name-brute,http-waf-fingerprint,http-auth,http-trace,http-iis-webdav-vuln,http-useragent-tester,http-vuln-cve2011-3368,http-userdir-enum,http-passwd,http-csrf,http-wordpress-enum,http-frontpage-login,http-dombased-xss,http-phpself-xss,http-sql-injection,http-drupal-enum-users,http-referer-checker,http-vuln-cve2009-3960,http-methods,http-open-redirect,http-vuln*,http-stored-xss,http-put,http-proxy-brute,http-rfi-spider,http-method-tamper,http-phpmyadmin-dir-traversal -oN ./results/{1}/{1}_http.nmap.{0} {1}".format(port, ip_address)
         results = subprocess.check_output(HTTPSCAN, shell=True)
         recon.logparsertxt(results)
