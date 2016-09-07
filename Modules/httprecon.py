944a6f8b437959788e466c550c43b5c346c0a02f
blindfuzzy <blindfuzzy@users.noreply.github.com>
2016-09-07 18:33:48 -0400

    purged Nikto code fixed mixing function

--- Modules/httprecon.py
+++ Modules/httprecon.py
@@ -39,13 +39,6 @@ try:
     recon.screenshot_http(ip_address, port, header)
     print('\033[1;33m[+]  Added screenshot to results for {0}:{1}...\033[1;m'.format(ip_address, port))
 
-    #NIKTOSCAN = "nikto -host {0} -p {1} -o ./results/{0}/{0}.{1}_nikto.txt".format(ip_address, port)
-    #print "\033[1;37m[-]  ----------------------------------------------------------------------------- \033[1;m"
-   # print('\033[1;37m[-]  |     Starting NIKTO scan for {0}:{1} \033[1;m'.format(ip_address, port))
-    #print "\033[1;37m[-]  ----------------------------------------------------------------------------- \033[1;m"
-    #resultsnikto = subprocess.check_output(NIKTOSCAN, shell=True)
-    #recon.logparsernikto(resultsnikto)
-
     SSLSCAN = "sslscan --no-colour {0}:{1}".format(ip_address, port)
     print "\033[1;37m[-]  ----------------------------------------------------------------------------- \033[1;m"
     print('\033[1;37m[-]  |     Starting SSL scan for {0}:{1} \033[1;m'.format(ip_address, port))
@@ -60,7 +53,7 @@ try:
 
     LBD = "lbd {0}".format(ip_address)
     print "\033[1;37m[-]  ----------------------------------------------------------------------------- \033[1;m"
-    print('\033[1;37m[-]  |     Starting Load Balancing scan for {0}:{1} \033[1;m'.format(ip_address))
+    print('\033[1;37m[-]  |     Starting Load Balancing scan for {0}:{1} \033[1;m'.format(ip_address, port))
     print "\033[1;37m[-]  ----------------------------------------------------------------------------- \033[1;m"
     resultsssl = subprocess.check_output(LBD, shell=True)
     recon.logparsertxt(resultsssl)
